f3a37e79e787eceac67ae8c8a3ad3aaca0f7563c
bluespada <pentingmain@gmail.com>
2025-08-30 08:28:31 +0800

    refactor: update smear-cursor
    Signed-off-by: bluespada <pentingmain@gmail.com>

--- lua/neocat/plugins/smear-cursor.lua
+++ lua/neocat/plugins/smear-cursor.lua
@@ -4,12 +4,14 @@ return {
         local smear = require'smear_cursor'
         smear.setup {
             stifness = 0.8,
-            trailing_stifness = 0.95,
-            stifness_insert_mode = 0.6,
-            trailing_stifness_insert_mode = 0.5,
-            damping = 0.2,
+            trailing_stifness = 0.8,
+            stifness_insert_mode = 0.9,
+            trailing_stifness_insert_mode = 0.9,
+            damping = 0.95,
             damping_insert_mode = 0.95,
-            distance_stop_animating = 0.5,
+            distance_stop_animating = 0.2,
+            matrix_pixel_threshold = 0.3,
+            time_interval = 7,
         }
     end
 }
